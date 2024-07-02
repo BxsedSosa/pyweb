bc5bc7a77082579a27a0389f62d61eb65a191b73
BxsedSosa <donovanxabad@gmail.com>
2024-07-02 08:14:00 -0700

    created post request to http server

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import tornado.web, tornado.ioloop
+import tornado.web, tornado.ioloop, json
+
+
+class mainRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("index.html")
 
 
 class basicRequestHandler(tornado.web.RequestHandler):
@@ -8,7 +13,17 @@ class basicRequestHandler(tornado.web.RequestHandler):
 
 class listRequestHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render("index.html")
+        fh = open("list.txt", "r")
+        fruits = fh.read().splitlines()
+        fh.close()
+        self.write(json.dumps(fruits, indent=4))
+
+    def post(self):
+        fruit = self.get_argument("fruit")
+        fh = open("list.txt", "a")
+        fh.write(f"{fruit}\n")
+        fh.close()
+        self.write(json.dumps({"message": "Fruit added"}))
 
 
 class queryParamRequestHandler(tornado.web.RequestHandler):
@@ -21,12 +36,18 @@ class queryParamRequestHandler(tornado.web.RequestHandler):
             self.write(f"{num} is not a valid int.")
 
 
+class resourceParamRequestHandler(tornado.web.RequestHandler):
+    def get(self, student_name, course_id):
+        self.write(f"Welcome {student_name} the course you are viewing is {course_id}")
+
+
 if __name__ == "__main__":
     app = tornado.web.Application(
         [
-            (r"/", basicRequestHandler),
-            (r"/animal", listRequestHandler),
+            (r"/", mainRequestHandler),
+            (r"/list", listRequestHandler),
             (r"/isEven", queryParamRequestHandler),
+            (r"/students/([a-z]+)/([0-9]+)", resourceParamRequestHandler),
         ]
     )
 
